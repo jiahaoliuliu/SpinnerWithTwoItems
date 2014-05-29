1918dd01a4db7f777032704a98431599a89e60b7
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-05-29 19:49:57 +0200

    Added simple spinner

--- src/com/jiahaoliuliu/spinnerwithtwoitems/MainActivity.java
+++ src/com/jiahaoliuliu/spinnerwithtwoitems/MainActivity.java
@@ -1,15 +1,20 @@
 package com.jiahaoliuliu.spinnerwithtwoitems;
 
 import com.jiahaoliuliu.spinnerwithtwoitems.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+
+	private Spinner simpleSpinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
+		simpleSpinner = (Spinner)findViewById(R.id.simpleSpinner);
 	}
 }
