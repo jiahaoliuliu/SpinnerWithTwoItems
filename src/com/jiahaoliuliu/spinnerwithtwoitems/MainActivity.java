8b3f859c93073d7745c2f68dff1db29e13451af6
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-05-29 19:09:19 +0200

    First commit

---
+++ src/com/jiahaoliuliu/spinnerwithtwoitems/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jiahaoliuliu.spinnerwithtwoitems;
+
+import com.jiahaoliuliu.spinnerwithtwoitems.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
